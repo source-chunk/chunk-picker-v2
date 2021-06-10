925e8dc1464674ea56843d81e4ba23560a034d53
source-chunk <52727627+source-chunk@users.noreply.github.com>
2021-06-10 10:10:34 -0500

    4.0.5 - Added patreon links

--- index.js
+++ index.js
@@ -695,7 +695,7 @@ let futureChunkData = {};
 // ----------------------------------------------------------
 
 // Recieve message from worker
-const myWorker = new Worker("./worker.js?v=4.0.4");
+const myWorker = new Worker("./worker.js?v=4.0.5");
 myWorker.onmessage = function(e) {
     workerOut--;
     workerOut < 0 && (workerOut = 0);
