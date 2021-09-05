5a56658834ce4225cc5043b4a6d3d166cd34f9f0
source-chunk <duosource.iron@gmail.com>
2021-09-04 20:37:57 -0500

    4.5.3 - Added sticker colors

--- index.js
+++ index.js
@@ -463,6 +463,7 @@ let settings = {
     "completedTaskStrikethrough": true,
     'randomStartAlways': false,
     "darkmode": false,
+    "defaultStickerColor": '#000000',
 };                                                                              // Current state of all settings
 
 let settingNames = {
@@ -478,6 +479,7 @@ let settingNames = {
     "completedTaskStrikethrough": "Cross-off chunk tasks as you complete them",
     "randomStartAlways": "Change the 'Pick Chunk' button to always be a 'Random Start' button; every chunk roll picks a random walkable chunk (that isn't already unlocked)",
     "darkmode": "Enable <b class='noscroll'>Dark Mode</b>",
+    "defaultStickerColor": "Change the default color of chunk stickers",
 };                                                                              // Descriptions of the settings
 
 let settingStructure = {
@@ -499,7 +501,8 @@ let settingStructure = {
         "highvis": true,
         "darkmode": true,
         "completedTaskStrikethrough": true,
-        "completedTaskColor": true
+        "completedTaskColor": true,
+        "defaultStickerColor": true
     }
 };                                                                              // Structure of the settings
 
@@ -730,6 +733,7 @@ let highestOverall = {};
 let savedBox = null;
 let stickered = {};
 let stickeredNotes = {};
+let stickeredColors = {};
 let stickerChoices = ['unset', 'skull', 'skull-crossbones', 'bomb', 'exclamation-circle', 'dice', 'poo', 'frown', 'grin-alt', 'heart', 'star', 'gem', 'award', 'crown', 'flag', 'asterisk', 'clock', 'hourglass', 'link', 'map-marker-alt', 'radiation-alt', 'shoe-prints', 'thumbs-down', 'thumbs-up', 'crow'];
 let stickerChoicesOsrs = ['attack', 'hitpoints', 'mining', 'strength', 'agility', 'smithing', 'defence', 'herblore', 'fishing', 'ranged', 'thieving', 'cooking', 'prayer', 'fletching', 'firemaking', 'magic', 'crafting', 'woodcutting', 'runecraft', 'slayer', 'farming', 'construction', 'hunter', 'quest', 'diary'];
 let savedStickerId;
@@ -752,7 +756,7 @@ let patreonMaps = {
 // ----------------------------------------------------------
 
 // Recieve message from worker
-const myWorker = new Worker("./worker.js?v=4.5.2");
+const myWorker = new Worker("./worker.js?v=4.5.3");
 myWorker.onmessage = function(e) {
     workerOut--;
     workerOut < 0 && (workerOut = 0);
@@ -3565,10 +3569,12 @@ var openStickers = function(id) {
     document.getElementById('sticker-data').scrollTop = 0;
     let chunkNickname = chunkInfo['chunks'].hasOwnProperty(id) ? chunkInfo['chunks'][id]['Nickname'] + ' ' : '';
     $('.sticker-chunk').text(chunkNickname + '(' + id + ')');
+    $('#sticker-notes-data > textarea').val(stickeredNotes[id]);
+    $('.sticker-color-picker').val(stickeredColors[id] || settings['defaultStickerColor']);
     stickerChoices.forEach(sticker => {
         let stickerName = sticker.split('-alt').join('').split('-').join(' ');
         if (sticker !== 'unset') {
-            $('.sticker-data').append(`<span class='noscroll sticker-option-container ${sticker}-tag' title='${stickerName.charAt(0).toUpperCase() + stickerName.slice(1)}' onclick="setSticker('${id}', '${sticker}')"><i class="noscroll fas fa-${sticker}" style="transform: scaleX(-1)"></i></span>`);
+            $('.sticker-data').append(`<span style='color:${$('.sticker-color-picker').val()}' class='noscroll sticker-option-container ${sticker}-tag' title='${stickerName.charAt(0).toUpperCase() + stickerName.slice(1)}' onclick="setSticker('${id}', '${sticker}')"><i class="noscroll fas fa-${sticker}" style="transform: scaleX(-1)"></i></span>`);
         } else {
             $('.sticker-data').append(`<span class='noscroll sticker-option-container unset-option' title='${stickerName.charAt(0).toUpperCase() + stickerName.slice(1)}' onclick="setSticker('${id}', '${sticker}')"><i class="noscroll fas fa-ban" style="transform: scaleX(-1)"></i></span>`);
         }
@@ -3585,7 +3591,6 @@ var openStickers = function(id) {
     } else {
         savedStickerSticker = 'unset';
     }
-    $('#sticker-notes-data > textarea').val(stickeredNotes[id]);
 }
 
 var submitSticker = function() {
@@ -3598,16 +3603,18 @@ var submitSticker = function() {
             $('.box > .chunkId:contains(' + id + ')').filter(function() { return parseInt($(this).text()) === parseInt(id); }).parent().children('.chunk-sticker').remove();
         }
         if (stickerChoices.includes(sticker)) {
-            $('.box > .chunkId:contains(' + id + ')').parent().append(`<span class='chunk-sticker permanent-sticker' onclick="openStickers(${id})"><i class="fas fa-${sticker}" style="transform: scaleX(-1)"></i></span>`);
+            $('.box > .chunkId:contains(' + id + ')').parent().append(`<span style='color:${$('.sticker-color-picker').val()}' class='chunk-sticker permanent-sticker' onclick="openStickers(${id})"><i class="fas fa-${sticker}" style="transform: scaleX(-1)"></i></span>`);
         } else if (stickerChoicesOsrs.includes(sticker)) {
             $('.box > .chunkId:contains(' + id + ')').parent().append(`<span class='chunk-sticker permanent-sticker' onclick="openStickers(${id})"><img src="./resources/SVG/${sticker}-osrs.svg"></span>`);
         }
         stickered[id] = sticker;
         stickeredNotes[id] = $('#sticker-notes-data > textarea').val();
+        stickeredColors[id] = $('.sticker-color-picker').val();
     } else {
         $('.box > .chunkId:contains(' + id + ')').filter(function() { return parseInt($(this).text()) === parseInt(id); }).parent().children('.chunk-sticker').remove();
         delete stickered[id];
         delete stickeredNotes[id];
+        delete stickeredColors[id];
         $('.box > .chunkId:contains(' + id + ')').parent().append(`<span class='chunk-sticker hidden-sticker' onclick="openStickers(${id})"><i class="fas fa-tag" style="transform: scaleX(-1)"></i></span>`);
     }
     $('.chunk-sticker').css('font-size', fontZoom * (3/2) + 'px');
@@ -3624,6 +3631,11 @@ var setSticker = function(id, sticker) {
     if (sticker !== 'unset') {
         $(`.sticker-data > .sticker-option-container.${sticker}-tag`).addClass('selected-sticker');
     }
+}
+
+// Changes the sticker options color
+var changeCurrentStickerColor = function() {
+    $('.sticker-option-container:not(.unset-option)').css('color', $('.sticker-color-picker').val());
 }
 
 // Opens the methods modal
@@ -4074,6 +4086,8 @@ var showSettings = function() {
         !!settingStructure[category] && Object.keys(settingStructure[category]).forEach(setting => {
             if (setting === 'completedTaskColor') {
                 $('.' + category.replaceAll(/\ /g, '_') + '-category').append(`<div class="setting ${setting.replaceAll(' ', '_').replaceAll('%', '').replaceAll(/\'/g, '-2H').replaceAll(/\&/g, '-2Z').replaceAll(/\(/g, '').replaceAll(/\)/g, '') + '-setting'} noscroll"><input class="challenge-color-rule" type="color" value="${settings[setting]}" onchange="changeChallengeColor()" /><i class="fas fa-undo-alt noscroll reset-challenge-color" title="Reset Color" onclick="resetChallengeColor()"></i><span class='noscroll extraspace'>` + settingNames[setting].replaceAll(/\%2E/g, '.').replaceAll(/\%2F/g, '#').replaceAll(/\%2G/g, '/') + '</span></div>');
+            } else if (setting === 'defaultStickerColor') {
+                $('.' + category.replaceAll(/\ /g, '_') + '-category').append(`<div class="setting ${setting.replaceAll(' ', '_').replaceAll('%', '').replaceAll(/\'/g, '-2H').replaceAll(/\&/g, '-2Z').replaceAll(/\(/g, '').replaceAll(/\)/g, '') + '-setting'} noscroll"><input class="sticker-color-rule" type="color" value="${settings[setting]}" onchange="changeDefaultStickerColor()" /><i class="fas fa-undo-alt noscroll reset-default-sticker-color" title="Reset Color" onclick="resetDefaultStickerColor()"></i><span class='noscroll extraspace'>` + settingNames[setting].replaceAll(/\%2E/g, '.').replaceAll(/\%2F/g, '#').replaceAll(/\%2G/g, '/') + '</span></div>');
             } else {
                 $('.' + category.replaceAll(/\ /g, '_') + '-category').append(`<div class="setting ${setting.replaceAll(' ', '_').replaceAll('%', '').replaceAll(/\'/g, '-2H').replaceAll(/\&/g, '-2Z').replaceAll(/\(/g, '').replaceAll(/\)/g, '') + '-setting'} noscroll"><input class="noscroll" type='checkbox' ${settings[setting] && "checked"} onclick="checkOffSettings()" ${(viewOnly || inEntry || locked || settingStructure[category][setting] === false) ? "disabled" : ''} /><span class='noscroll'>` + settingNames[setting].replaceAll(/\%2E/g, '.').replaceAll(/\%2F/g, '#').replaceAll(/\%2G/g, '/') + '</span></div>');
             }
@@ -4099,6 +4113,18 @@ var changeChallengeColor = function() {
 var resetChallengeColor = function() {
     $('.challenge-color-rule').val('#0D8219');
     changeChallengeColor();
+}
+
+// Changes the default sticker color
+var changeDefaultStickerColor = function() {
+    $('.sticker-color-rule').length && (settings['defaultStickerColor'] = $('.sticker-color-rule').val());
+    setData();
+}
+
+// Resets the default sticker color
+var resetDefaultStickerColor = function() {
+    $('.sticker-color-rule').val('#000000');
+    changeDefaultStickerColor();
 }
 
 // Selects correct active context menu item
@@ -4390,7 +4416,7 @@ var checkOffRules = function(didRedo, startup) {
 // Marks checked off settings
 var checkOffSettings = function(startup) {
     Object.keys(settings).forEach(setting => {
-        if (setting !== 'completedTaskColor' && settingNames.hasOwnProperty(setting)) {
+        if (setting !== 'completedTaskColor' && setting !== 'defaultStickerColor' && settingNames.hasOwnProperty(setting)) {
             settings[setting] = $('.' + setting.replaceAll(' ', '_').replaceAll('%', '').replaceAll(/\'/g, '-2H').replaceAll(/\&/g, '-2Z').replaceAll(/\(/g, '').replaceAll(/\)/g, '') + '-setting > input').prop('checked');
         }
     });
@@ -4724,15 +4750,17 @@ var loadData = function(startup) {
             $('.chunk-sticker').remove();
             chunks && chunks['stickered'] && Object.keys(chunks['stickered']).forEach(function(id) {
                 if (stickerChoices.includes(chunks['stickered'][id])) {
-                    $('.box > .chunkId:contains(' + id + ')').filter(function() { return parseInt($(this).text()) === parseInt(id); }).parent().append(`<span class='chunk-sticker permanent-sticker' onclick="openStickers(${id})"><i class="fas fa-${chunks['stickered'][id]}" style="transform: scaleX(-1)"></i></span>`);
+                    $('.box > .chunkId:contains(' + id + ')').filter(function() { return parseInt($(this).text()) === parseInt(id); }).parent().append(`<span style='color:${chunks['stickeredColors'][id] || settings['defaultStickerColor'] || '#000000'}' class='chunk-sticker permanent-sticker' onclick="openStickers(${id})"><i class="fas fa-${chunks['stickered'][id]}" style="transform: scaleX(-1)"></i></span>`);
                 } else if (stickerChoicesOsrs.includes(chunks['stickered'][id])) {
                     $('.box > .chunkId:contains(' + id + ')').filter(function() { return parseInt($(this).text()) === parseInt(id); }).parent().append(`<span class='chunk-sticker permanent-sticker' onclick="openStickers(${id})"><img src="./resources/SVG/${chunks['stickered'][id]}-osrs.svg"></span>`);
                 }
             });
             $('.chunk-sticker').css('font-size', fontZoom * (3/2) + 'px');
             $('.chunk-sticker > img').parent().css('width', fontZoom * (3/2) + 'px');
+            
             stickered = (chunks ? chunks['stickered'] : {}) || {};
             stickeredNotes = (chunks ? chunks['stickeredNotes'] : {}) || {};
+            stickeredColors = (chunks ? chunks['stickeredColors'] : {}) || {};
     
             if (picking) {
                 $('.unpick').css({'opacity': 0, 'cursor': 'default'}).prop('disabled', true).hide();
@@ -4801,7 +4829,7 @@ var setData = function() {
                     return;
                 });
             } else {
-                myRef.child('settings').update({'neighbors': autoSelectNeighbors, 'remove': autoRemoveSelected, 'roll2': roll2On, 'unpick': unpickOn, 'recent': recentOn, 'highscoreEnabled': highscoreEnabled, 'chunkTasks': chunkTasksOn, 'completedTaskColor': settings['completedTaskColor'], 'completedTaskStrikethrough': settings['completedTaskStrikethrough'], 'randomStartAlways': settings['randomStartAlways']});
+                myRef.child('settings').update({'neighbors': autoSelectNeighbors, 'remove': autoRemoveSelected, 'roll2': roll2On, 'unpick': unpickOn, 'recent': recentOn, 'highscoreEnabled': highscoreEnabled, 'chunkTasks': chunkTasksOn, 'completedTaskColor': settings['completedTaskColor'], 'completedTaskStrikethrough': settings['completedTaskStrikethrough'], 'randomStartAlways': settings['randomStartAlways'], 'defaultStickerColor': settings['defaultStickerColor']});
                 Object.keys(rules).forEach(rule => {
                     if (rules[rule] === undefined) {
                         rules[rule] = false;
@@ -4856,6 +4884,7 @@ var setData = function() {
 
                 myRef.child('chunks/stickered').set(stickered);
                 myRef.child('chunks/stickeredNotes').set(stickeredNotes);
+                myRef.child('chunks/stickeredColors').set(stickeredColors);
 
                 highscoreEnabled && databaseRef.child('highscores/skills/Unlocked Chunks/' + mid).update({
                     mid: mid,
@@ -4921,6 +4950,7 @@ var setData = function() {
 
                 myRef.child('chunks/stickered').set(stickered);
                 myRef.child('chunks/stickeredNotes').set(stickeredNotes);
+                myRef.child('chunks/stickeredColors').set(stickeredColors);
 
                 highscoreEnabled && databaseRef.child('highscores/skills/Unlocked Chunks/' + mid).update({
                     mid: mid,
