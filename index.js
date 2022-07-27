75cd258f63121faec336c5d1001e15fc70ce35d1
source-chunk <duosource.iron@gmail.com>
2022-07-27 08:28:32 -0500

    4.18.18 - Fixed passiveSkill missing bug

--- index.js
+++ index.js
@@ -2461,7 +2461,7 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-const myWorker = new Worker("./worker.js?v=4.18.17");
+const myWorker = new Worker("./worker.js?v=4.18.18");
 myWorker.onmessage = function(e) {
     if (e.data[0] === 'error') {
         $('.panel-active > .calculating > .inner-loading-bar').css('background-color', 'red');
