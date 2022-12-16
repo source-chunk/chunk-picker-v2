6539eaa37663c039b15f0b7363f74d4ec29fff0e
source-chunk <duosource.iron@gmail.com>
2022-12-15 18:37:07 -0600

    5.1.29 - Removed log statements

--- worker.js
+++ worker.js
@@ -251,13 +251,13 @@ onmessage = function(e) {
         postMessage('90%');
         calcBIS();
         postMessage('100%');
-        console.log(globalValids);
+        //console.log(globalValids);
 
         let tempChallengeArr;
         type === 'current' && (tempChallengeArr = calcCurrentChallenges2());
 
-        console.log(nonValids);
-        console.log(baseChunkData);
+        //console.log(nonValids);
+        //console.log(baseChunkData);
         
         postMessage([type, globalValids, baseChunkData, chunkInfo, highestCurrent, tempChallengeArr, type === 'current' ? questPointTotal : 0, highestOverall, type === 'current' ? dropRatesGlobal : {}, questProgress, diaryProgress, skillQuestXp, chunks]);
     } catch (err) {
