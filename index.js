8f240252c11ca2e1b71119436b90817f376a888f
source chunk <duosource.iron@gmail.com>
2024-04-29 16:22:18 -0600

    6.1.20 - Updated bannedIds

--- index.js
+++ index.js
@@ -1320,7 +1320,7 @@ let expandChallengeStr = '';
 let detailsStack = [];
 let touchTime = 0;
 
-let currentVersion = '6.1.19';
+let currentVersion = '6.1.20';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1444,7 +1444,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.1.19";
+mapImg.src = "osrs_world_map.png?v=6.1.20";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -2880,7 +2880,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.1.19");
+        myWorker = new Worker("./worker.js?v=6.1.20");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections']]);
         workerOut = 1;
@@ -3142,8 +3142,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.1.19");
-let myWorker2 = new Worker("./worker.js?v=6.1.19");
+let myWorker = new Worker("./worker.js?v=6.1.20");
+let myWorker2 = new Worker("./worker.js?v=6.1.20");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -5825,7 +5825,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.1.19");
+    myWorker2 = new Worker("./worker.js?v=6.1.20");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections']]);
     workerOut++;
@@ -10404,7 +10404,7 @@ let setData = function() {
 
 // Rolls until a new, unique map id is found
 let rollMID = function(count) {
-    const bannedIds = ['nig', 'anig', 'nigg', 'nigr', 'nggr', 'nigs', 'ngr', 'ngrs', 'niga', 'ngga', 'nigo', 'nigz', 'ngrz', 'gook', 'nazi', 'htlr', 'fag', 'afag', 'fgt', 'afgt', 'fgts', 'fagg', 'fago', 'fags', 'faag', 'ffag', 'fagz', 'fagt', 'dyke'];
+    const bannedIds = ['nig', 'anig', 'nigg', 'nigr', 'nggr', 'nigs', 'ngr', 'ngrs', 'niga', 'ngga', 'nigo', 'nigz', 'ngrz', 'gook', 'nazi', 'htlr', 'fag', 'afag', 'fgt', 'afgt', 'fgts', 'fagg', 'fago', 'fags', 'faag', 'ffag', 'fagz', 'fagt', 'dyke', 'acab'];
     let rollMidCount = count || 0;
     let char1, char2, char3, char4, charSet;
     let badNums = true;
