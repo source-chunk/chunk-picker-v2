51ebb94303af11e452465ee83c361056e8ad3f5b
source chunk <duosource.iron@gmail.com>
2024-06-11 08:50:14 -0600

    6.2.14 - Added map creation date

--- index.js
+++ index.js
@@ -1329,7 +1329,7 @@ let expandChallengeStr = '';
 let detailsStack = [];
 let touchTime = 0;
 
-let currentVersion = '6.2.13';
+let currentVersion = '6.2.14';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1468,7 +1468,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.2.13";
+mapImg.src = "osrs_world_map.png?v=6.2.14";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3103,7 +3103,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.2.13");
+        myWorker = new Worker("./worker.js?v=6.2.14");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
         workerOut = 1;
@@ -3386,8 +3386,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.2.13");
-let myWorker2 = new Worker("./worker.js?v=6.2.13");
+let myWorker = new Worker("./worker.js?v=6.2.14");
+let myWorker2 = new Worker("./worker.js?v=6.2.14");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -6078,7 +6078,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.2.13");
+    myWorker2 = new Worker("./worker.js?v=6.2.14");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
     workerOut++;
@@ -9659,6 +9659,16 @@ let showChunkHistory = function() {
             $('#chunkhistory-data-inner').append(`<div class="history-item ${chunkOrder[time] + '-chunk-history-item'} noscroll"><span class='noscroll item1'>${"<b class='noscroll'>" + tempDate.toLocaleDateString([], { year: 'numeric', month: 'long', day: '2-digit' }) + '</b> (' + tempDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' }) + ') '}</span><span class='noscroll item2'>${"<b class='noscroll'>" + ((chunkInfo['chunks'].hasOwnProperty(parseInt(chunkOrder[time])) && chunkInfo['chunks'][parseInt(chunkOrder[time])].hasOwnProperty('Nickname')) ? chunkInfo['chunks'][parseInt(chunkOrder[time])]['Nickname'] : 'Unknown chunk') + '</b>' + ' (' + chunkOrder[time] + ')'}</span></div>`);
         }
     });
+    databaseRef.child('mapCreationTimes/' + mid).once('value', function(snap) {
+        if (!!snap.val()) {
+            tempDate.setTime(Object.keys(chunkOrder).sort(function(a, b) { return a - b })[0]);
+            if (!chunkOrder || Object.keys(chunkOrder).length === 0 || snap.val() < Object.keys(chunkOrder).sort(function(a, b) { return a - b })[0]) {
+                $('#chunkhistory-data-inner').append(`<div class="creation-date noscroll"><span class='noscroll'>${"<b class='noscroll'>" + 'Map Creation: ' + '</b>'}</span><span class='noscroll'>${new Date(snap.val()).toLocaleDateString([], { year: 'numeric', month: 'long', day: '2-digit' }) + ' (' + new Date(snap.val()).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' }) + ') [' + Math.floor((Date.now() - new Date(snap.val()).getTime()) / (1000 * 3600 * 24)) + ' Days Ago]'}</span></div>`);
+            } else {
+                $('#chunkhistory-data-inner').append(`<div class="creation-date noscroll"><span class='noscroll'>${"<b class='noscroll'>" + 'Approximate Map Creation: ' + '</b>'}</span><span class='noscroll'>${tempDate.toLocaleDateString([], { year: 'numeric', month: 'long', day: '2-digit' }) + ' (' + tempDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' }) + ') [' + Math.floor((Date.now() - tempDate.getTime()) / (1000 * 3600 * 24)) + ' Days Ago]'}</span></div>`);
+            }
+        }
+    });
     // Graph
     if (Object.keys(newChunkOrder).length >= 3 && (Date.now() - Object.keys(chunkOrder).sort(function(a, b) { return a - b })[0] >= 300000 && !!tempChunks['unlocked'] && Object.keys(tempChunks['unlocked']).length >= 3)) {
         $('.canvas-graph-outer').show();
@@ -9726,8 +9736,8 @@ let showChunkHistory = function() {
         $('.canvas-graph-outer').hide();
         $('.average-rolltime-title').hide().text('');
     }
-    document.getElementById('chunkhistory-data-inner').scrollTop = 0;
     $('#myModal18').show();
+    document.getElementById('chunkhistory-data').scrollTop = 0;
     modalOutsideTime = Date.now();
     settingsMenu();
 }
@@ -10860,6 +10870,7 @@ let rollMID = function(count) {
                             temp.uid = userCredential.user.uid;
                             databaseRef.child('maps/' + charSet).set(temp);
                             databaseRef.child('mapids/' + charSet).set(true);
+                            databaseRef.child('mapCreationTimes/' + charSet).set(new Date(userCredential.user.metadata.creationTime).getTime());
                             $('#newmid').text(charSet.toUpperCase());
                             $('.link').prop('href', 'https://source-chunk.github.io/chunk-picker-v2/?' + charSet).text('https://source-chunk.github.io/chunk-picker-v2/?' + charSet);
                         });
