fc4691f1f4aea3cc7b24a12ee772b8641a5da9b4
source chunk <duosource.iron@gmail.com>
2024-06-20 11:20:35 -0600

    6.2.20.1 - Added POH Rooms rule

--- index.js
+++ index.js
@@ -414,6 +414,7 @@ let rules = {
     "Boosting": false,
     "Superheat Furnace": false,
     "Partial Products": false,
+    "POH Rooms": false,
 };                                                                              // List of rules and their on/off state
 
 let ruleNames = {
@@ -505,7 +506,8 @@ let ruleNames = {
     "Cleaning Herbs": "Cleaning grimy herbs/making (unf) potions can count as chunk tasks",
     "Boosting": "Allow skill boosts to be considered for skill tasks",
     "Superheat Furnace": "Allow the Superheat Item spell to act as a furnace for training Smithing <span class='rule-asterisk noscroll'>*</span>",
-    "Partial Products": "Require making of partial products as a skill task (partially assembling pies - requires a skill level to make, but gives no xp) <span class='rule-asterisk noscroll'>*</span>"
+    "Partial Products": "Require making of partial products as a skill task (partially assembling pies - requires a skill level to make, but gives no xp) <span class='rule-asterisk noscroll'>*</span>",
+    "POH Rooms": "Allow building rooms to count as Construction skill tasks"
 };                                                                              // List of rule definitions
 
 let rulePresets = {
@@ -594,6 +596,7 @@ let rulePresets = {
         "ForestryXp": true,
         "Partial Products": true,
         "Money Unlockables": true,
+        "POH Rooms": true,
     },
     "Supreme Chunker": {
         "Skillcape": true,
@@ -661,6 +664,7 @@ let rulePresets = {
         "Superheat Furnace": true,
         "Forestry": true,
         "ForestryXp": true,
+        "POH Rooms": true,
     },
 };                                                                              // List of rules that are part of each preset
 
@@ -700,7 +704,8 @@ let ruleStructure = {
         "InsidePOH Primary": true,
         "InsidePOH": true,
         "Construction Milestone": true,
-        "Construction Minigame": true
+        "Construction Minigame": true,
+        "POH Rooms": true
     },
     "Farming": {
         "Normal Farming": true,
@@ -1330,7 +1335,7 @@ let detailsStack = [];
 let touchTime = 0;
 let pluginOutput = null;
 
-let currentVersion = '6.2.20';
+let currentVersion = '6.2.20.1';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1470,7 +1475,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.2.20";
+mapImg.src = "osrs_world_map.png?v=6.2.20.1";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3105,7 +3110,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.2.20");
+        myWorker = new Worker("./worker.js?v=6.2.20.1");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
         workerOut = 1;
@@ -3388,8 +3393,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.2.20");
-let myWorker2 = new Worker("./worker.js?v=6.2.20");
+let myWorker = new Worker("./worker.js?v=6.2.20.1");
+let myWorker2 = new Worker("./worker.js?v=6.2.20.1");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -6142,7 +6147,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.2.20");
+    myWorker2 = new Worker("./worker.js?v=6.2.20.1");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
     workerOut++;
