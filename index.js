f68ddf0ab10aabe65ee8d0488efc597fb60346a3
source chunk <duosource.iron@gmail.com>
2024-07-08 16:52:17 -0600

    6.2.29 - Fixed empty string in rules value bug

--- index.js
+++ index.js
@@ -1343,7 +1343,7 @@ let detailsStack = [];
 let touchTime = 0;
 let pluginOutput = null;
 
-let currentVersion = '6.2.28.3';
+let currentVersion = '6.2.29';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1483,7 +1483,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.2.28.3";
+mapImg.src = "osrs_world_map.png?v=6.2.29";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3118,7 +3118,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.2.28.3");
+        myWorker = new Worker("./worker.js?v=6.2.29");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
         workerOut = 1;
@@ -3401,8 +3401,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.2.28.3");
-let myWorker2 = new Worker("./worker.js?v=6.2.28.3");
+let myWorker = new Worker("./worker.js?v=6.2.29");
+let myWorker2 = new Worker("./worker.js?v=6.2.29");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -6157,7 +6157,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.2.28.3");
+    myWorker2 = new Worker("./worker.js?v=6.2.29");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
     workerOut++;
@@ -10138,17 +10138,17 @@ let checkOffRules = function(didRedo, startup) {
             redo = true;
         }
         if (rule === 'Kill X Amount') {
-            if ($(extraFilter + '.x-num-input').val() < 1) {
+            if ($(extraFilter + '.x-num-input').val() < 1 || !$(extraFilter + '.x-num-input').val()) {
                 $(extraFilter + '.x-num-input').val(1);
             }
             rules[rule] = $(extraFilter + '.x-num-input').val();
         } else if (rule === 'Rare Drop Amount') {
-            if ($(extraFilter + '.rare-num-input').val() < 0) {
+            if ($(extraFilter + '.rare-num-input').val() < 0 || !$(extraFilter + '.rare-num-input').val()) {
                 $(extraFilter + '.rare-num-input').val(0);
             }
             rules[rule] = $(extraFilter + '.rare-num-input').val();
         } else if (rule === 'Secondary Primary Amount') {
-            if ($(extraFilter + '.secondary-primary-input').val() < 0) {
+            if ($(extraFilter + '.secondary-primary-input').val() < 0 || !$(extraFilter + '.secondary-primary-input').val()) {
                 $(extraFilter + '.secondary-primary-input').val(0);
             }
             rules[rule] = $(extraFilter + '.secondary-primary-input').val();
