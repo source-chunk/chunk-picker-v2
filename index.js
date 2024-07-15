b0721699fb3911773a0286bd86aa99cfddb4319b
source chunk <duosource.iron@gmail.com>
2024-07-15 09:09:14 -0600

    6.2.29.2 - Fixed Runecraft processing flag

--- index.js
+++ index.js
@@ -1110,7 +1110,7 @@ let processingSkill = {
     "Hitpoints": false,
     "Ranged": false,
     "Prayer": false,
-    "Runecraft": false,
+    "Runecraft": true,
     "Magic": true,
     "Farming": false,
     "Herblore": true,
@@ -1125,7 +1125,7 @@ let processingSkill = {
     "Crafting": true,
     "Agility": false,
     "Construction": true,
-    "Combat": true,
+    "Combat": false,
     "Quest": false,
     "Diary": false,
     "Nonskill": false,
@@ -1343,7 +1343,7 @@ let detailsStack = [];
 let touchTime = 0;
 let pluginOutput = null;
 
-let currentVersion = '6.2.29.1';
+let currentVersion = '6.2.29.2';
 let patchNotesVersion = '6.0.0';
 
 // Patreon Test Server Data
@@ -1363,6 +1363,7 @@ let patreonMaps = {
     'wckd': true, // ChunkLok9090
     'shrw': true, // VarrVictini
     'ggay': true, // Sir Var Lock
+    'lums': true, // lumSupreme
 };
 
 let roll5Mid = 'rfr'; // Semanari
@@ -1483,7 +1484,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.2.29.1";
+mapImg.src = "osrs_world_map.png?v=6.2.29.2";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3118,7 +3119,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.2.29.1");
+        myWorker = new Worker("./worker.js?v=6.2.29.2");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
         workerOut = 1;
@@ -3401,8 +3402,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.2.29.1");
-let myWorker2 = new Worker("./worker.js?v=6.2.29.1");
+let myWorker = new Worker("./worker.js?v=6.2.29.2");
+let myWorker2 = new Worker("./worker.js?v=6.2.29.2");
 let workerOnMessage = function(e) {
     if (lastUpdated + 2000000 < Date.now() && !hasUpdate) {
         lastUpdated = Date.now();
@@ -6157,7 +6158,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.2.29.1");
+    myWorker2 = new Worker("./worker.js?v=6.2.29.2");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill]);
     workerOut++;
