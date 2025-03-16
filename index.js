aceee1a9fd876c2abaa8ce41b5ef7b169ac53c33
source chunk <duosource.iron@gmail.com>
2025-03-16 11:46:28 -0600

    6.6.10 - Fixed slayerLocked slayer monster bug

--- index.js
+++ index.js
@@ -1399,7 +1399,7 @@ let topbarElements = {
     'Sandbox Mode': `<div><span class='noscroll' onclick="enableTestMode()"><i class="gosandbox fa-solid fa-flask" title='Sandbox Mode'></i></span></div>`,
 };
 
-let currentVersion = '6.6.9';
+let currentVersion = '6.6.10';
 let patchNotesVersion = '6.4.0';
 let updateLevel = 'difference';
 
@@ -1560,7 +1560,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.6.9";
+mapImg.src = "osrs_world_map.png?v=6.6.10";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3243,7 +3243,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.6.9");
+        myWorker = new Worker("./worker.js?v=6.6.10");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
         workerOut = 1;
@@ -3546,8 +3546,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.6.9");
-let myWorker2 = new Worker("./worker.js?v=6.6.9");
+let myWorker = new Worker("./worker.js?v=6.6.10");
+let myWorker2 = new Worker("./worker.js?v=6.6.10");
 let workerOnMessage = function(e) {
     if (e.data[0] === 'reload') {
         window.location.reload();
@@ -6380,7 +6380,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.6.9");
+    myWorker2 = new Worker("./worker.js?v=6.6.10");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
     workerOut++;
@@ -8529,15 +8529,15 @@ let addPassiveSkill = function(close, skill) {
 let unlockSlayer = function() {
     slayerLocked = null;
     setData();
-    calcCurrentChallengesCanvas(true);
-    openHighest2();
+    calcCurrentChallengesCanvas(true, true);
+    highest2ModalOpen && openHighest2();
 }
 
 // Checks if slayer locked monster is unlocked
 let checkSlayerLocked = function() {
     if (!!slayerLocked && !!slayerTasks && slayerTasks.hasOwnProperty(slayerLocked['monster'])) {
         !!slayerTasks[slayerLocked['monster']] && Object.keys(slayerTasks[slayerLocked['monster']]).forEach((monster) => {
-            if (!!baseChunkData['monsters'] && baseChunkData['monsters'].hasOwnProperty(monster)) {
+            if (!!baseChunkData['monsters'] && baseChunkData['monsters'].hasOwnProperty(monster) && (!chunkInfo['slayerMonsters'].hasOwnProperty(monster) || chunkInfo['slayerMonsters'][monster] <= slayerLocked['level'])) {
                 unlockSlayer();
                 return;
             }
@@ -8549,8 +8549,8 @@ let checkSlayerLocked = function() {
 let unlockConstruction = function() {
     constructionLocked = null;
     setData();
-    calcCurrentChallengesCanvas(true);
-    openHighest2();
+    calcCurrentChallengesCanvas(true, true);
+    highest2ModalOpen && openHighest2();
 }
 
 // Checks if construction locked chunk for Mahogany Homes is unlocked
