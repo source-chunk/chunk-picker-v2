025271f867b904c967d51715d1de83a9cd59c54e
source chunk <duosource.iron@gmail.com>
2025-04-06 12:08:53 -0600

    6.6.12 - Mobile task search fixes

--- index.js
+++ index.js
@@ -1401,7 +1401,7 @@ let topbarElements = {
     'Sandbox Mode': `<div><span class='noscroll' onclick="enableTestMode()"><i class="gosandbox fa-solid fa-flask" title='Sandbox Mode'></i></span></div>`,
 };
 
-let currentVersion = '6.6.11';
+let currentVersion = '6.6.12';
 let patchNotesVersion = '6.4.0';
 let updateLevel = 'difference';
 
@@ -1565,7 +1565,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.6.11";
+mapImg.src = "osrs_world_map.png?v=6.6.12";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3263,7 +3263,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.6.11");
+        myWorker = new Worker("./worker.js?v=6.6.12");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
         workerOut = 1;
@@ -3566,8 +3566,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.6.11");
-let myWorker2 = new Worker("./worker.js?v=6.6.11");
+let myWorker = new Worker("./worker.js?v=6.6.12");
+let myWorker2 = new Worker("./worker.js?v=6.6.12");
 let workerOnMessage = function(e) {
     if (e.data[0] === 'reload') {
         window.location.reload();
@@ -5464,9 +5464,7 @@ let toggleChunkTasks = function(value, extra) {
 
 // Toggles the chunk tasks searchbar
 let toggleTaskSearchbar = function(value, extra) {
-    if (!onMobile) {
-        value ? $('#searchActiveTasks').show() : $('#searchActiveTasks').hide();
-    }
+    value ? $('#searchActiveTasks').show() : $('#searchActiveTasks').hide();
     extra !== 'startup' && !locked && setData();
 }
 
@@ -5729,6 +5727,10 @@ let openMobileTasks = function() {
         $('.gomobiletasks').toggleClass('fa-tasks').toggleClass('fa-map');
         $('.gomobiletasks').attr('title', $('.gomobiletasks').hasClass('fa-tasks') ? 'Tasks' : 'Map');
         mobileTasksOpen = !$('.gomobiletasks').hasClass('fa-tasks');
+        if (!mobileTasksOpen && removeCanvasDarkness) {
+            removeCanvasDarkness = false;
+            $('#canvas').css('filter', 'brightness(1)');
+        }
     }
 }
 
@@ -6410,7 +6412,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.6.11");
+    myWorker2 = new Worker("./worker.js?v=6.6.12");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
     workerOut++;
