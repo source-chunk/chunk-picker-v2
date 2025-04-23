5ba461be6095e2fc0e2a61040eefa8e6bc2d0bbd
source chunk <duosource.iron@gmail.com>
2025-04-23 06:46:08 -0600

    6.6.17 - Fixed magic required primary runes

--- worker.js
+++ worker.js
@@ -3633,7 +3633,7 @@ let calcChallengesWork = function(chunks, baseChunkData, oldTempItemSkill) {
                             itSecondary = false;
                             if (it.includes('*') && Object.keys(items[plus]).filter((source) => { return !items[plus][source].includes('secondary-') || items[plus][source].includes('primary-') || items[plus][source] === 'shop' }).length > 0) {
                                 return true;
-                            } else {
+                            } else if (it.includes('*')) {
                                 tempValid = false;
                             }
                         });
