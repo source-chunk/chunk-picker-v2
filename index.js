352a0589a9ce00e192cc0d1d9ecbce1578290c98
source chunk <duosource.iron@gmail.com>
2025-05-04 15:05:57 -0600

    6.6.19.1 - Fixed joining pools kick date

--- index.js
+++ index.js
@@ -1401,7 +1401,7 @@ let topbarElements = {
     'Sandbox Mode': `<div><span class='noscroll' onclick="enableTestMode()"><i class="gosandbox fa-solid fa-flask" title='Sandbox Mode'></i></span></div>`,
 };
 
-let currentVersion = '6.6.19';
+let currentVersion = '6.6.19.1';
 let patchNotesVersion = '6.4.0';
 let updateLevel = 'difference';
 
@@ -1565,7 +1565,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.6.19";
+mapImg.src = "osrs_world_map.png?v=6.6.19.1";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3263,7 +3263,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.6.19");
+        myWorker = new Worker("./worker.js?v=6.6.19.1");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
         workerOut = 1;
@@ -3566,8 +3566,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.6.19");
-let myWorker2 = new Worker("./worker.js?v=6.6.19");
+let myWorker = new Worker("./worker.js?v=6.6.19.1");
+let myWorker2 = new Worker("./worker.js?v=6.6.19.1");
 let workerOnMessage = function(e) {
     if (e.data[0] === 'reload') {
         window.location.reload();
@@ -6422,7 +6422,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.6.19");
+    myWorker2 = new Worker("./worker.js?v=6.6.19.1");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
     workerOut++;
@@ -7959,7 +7959,7 @@ let loadPoolsData = function() {
         let upgradeText = splitData[7].replace(/"([^"]*?),([^"]*?)"/g, (match, part1, part2) => {
             return `"${part1}|${part2}"`;
         }).split(',')[13].replaceAll('"', '').replaceAll('|', ',');
-        let kickText = splitData[7].replace(/"([^"]*?),([^"]*?)"/g, (match, part1, part2) => {
+        let kickText = splitData[10].replace(/"([^"]*?),([^"]*?)"/g, (match, part1, part2) => {
             return `"${part1}|${part2}"`;
         }).split(',')[13].replaceAll('"', '').replaceAll('|', ',');
         $('.pools-period-b').text(upgradeText);
