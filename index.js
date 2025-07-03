422dcd11ab814c0fa451e4d63ff6c840cc5d3900
source chunk <duosource.iron@gmail.com>
2025-07-03 09:28:07 -0600

    6.7.3 - Fixed backlog training method missing note

--- index.js
+++ index.js
@@ -1407,7 +1407,7 @@ let topbarElements = {
     'Sandbox Mode': `<div><span class='noscroll' onclick="enableTestMode()"><i class="gosandbox fa-solid fa-flask" title='Sandbox Mode'></i></span></div>`,
 };
 
-let currentVersion = '6.7.2';
+let currentVersion = '6.7.3';
 let patchNotesVersion = '6.4.0';
 let updateLevel = 'difference';
 
@@ -1572,7 +1572,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.7.2";
+mapImg.src = "osrs_world_map.png?v=6.7.3";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3277,7 +3277,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.7.2");
+        myWorker = new Worker("./worker.js?v=6.7.3");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage(['current', tempChunks['unlocked'], rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
         workersOut['current'] = true;
@@ -3581,8 +3581,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.7.2");
-let myWorker2 = new Worker("./worker.js?v=6.7.2");
+let myWorker = new Worker("./worker.js?v=6.7.3");
+let myWorker2 = new Worker("./worker.js?v=6.7.3");
 let workerOnMessage = function(e) {
     if (e.data[0] === 'reload') {
         window.location.reload();
@@ -6426,7 +6426,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.7.2");
+    myWorker2 = new Worker("./worker.js?v=6.7.3");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage(['future', chunks, rules, chunkInfo, skillNames, processingSkill, maybePrimary, combatSkills, monstersPlus, objectsPlus, chunksPlus, itemsPlus, mixPlus, npcsPlus, tasksPlus, tools, elementalRunes, manualTasks, completedChallenges, backlog, "1/" + rules['Rare Drop Amount'], universalPrimary, elementalStaves, rangedItems, boneItems, highestCurrent, dropTables, possibleAreas, randomLoot, magicTools, bossLogs, bossMonsters, minigameShops, manualEquipment, checkedChallenges, backloggedSources, altChallenges, manualMonsters, slayerLocked, passiveSkill, f2pSkills, assignedXpRewards, mid === diary2Tier, manualAreas, "1/" + rules['Secondary Primary Amount'], constructionLocked, mid === manualAreasOnly, tempSections, settings['optOutSections'], maxSkill, userTasks, manualPrimary, updateLevel]);
     workersOut['future'] = infoLockedId;
@@ -10940,7 +10940,7 @@ let switchManualPrimaryContext = function(opt) {
 // Selects correct manual primary context menu item
 let switchTrainingMethodsContext = function(opt) {
     if (opt !== 'cancel') {
-        backlogChallenge(trainingMethodsContextMenuChallenge, trainingMethodsContextMenuSkill);
+        backlogChallenge(trainingMethodsContextMenuChallenge, trainingMethodsContextMenuSkill, '');
         viewPrimaryMethodsOrTasks(trainingMethodsContextMenuSkill, false);
     }
     $(".trainingmethods-context-menu").hide(100);
