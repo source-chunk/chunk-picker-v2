1361550346b0b7382ab2e57b85407a0e74be4856
source chunk <duosource.iron@gmail.com>
2026-08-28 13:26:40 -0600

    6.9.66 - talismanExit error fix

--- index.js
+++ index.js
@@ -1538,7 +1538,7 @@ let topbarElements = {
     'Sandbox Mode': `<div><span class='noscroll' onclick="enableTestMode()"><i class="gosandbox fa-solid fa-flask" title='Sandbox Mode'></i></span></div>`,
 };
 
-let currentVersion = '6.9.65';
+let currentVersion = '6.9.66';
 let currentEnforcedVersion = '6.9.45';
 let patchNotesVersion = '6.9.12';
 let updateLevel = 'unconnected-areas';
@@ -1711,7 +1711,7 @@ mapImg.addEventListener("load", e => {
         centerCanvas('quick');
     }
 });
-mapImg.src = "osrs_world_map.png?v=6.9.65";
+mapImg.src = "osrs_world_map.png?v=6.9.66";
 
 // Rounded rectangle
 CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
@@ -3639,7 +3639,7 @@ let calcCurrentChallengesCanvas = function(useOld, proceed, fromLoadData, inputT
         setCalculating('.panel-active', useOld);
         setCurrentChallenges(['No tasks currently backlogged.'], ['No tasks currently completed.'], true, true);
         myWorker.terminate();
-        myWorker = new Worker("./worker.js?v=6.9.65");
+        myWorker = new Worker("./worker.js?v=6.9.66");
         myWorker.onmessage = workerOnMessage;
         myWorker.postMessage({
             type: 'current',
@@ -4000,8 +4000,8 @@ $(document).ready(function() {
 // ------------------------------------------------------------
 
 // Recieve message from worker
-let myWorker = new Worker("./worker.js?v=6.9.65");
-let myWorker2 = new Worker("./worker.js?v=6.9.65");
+let myWorker = new Worker("./worker.js?v=6.9.66");
+let myWorker2 = new Worker("./worker.js?v=6.9.66");
 let workerOnMessage = function(e) {
     if (e.data.type === 'reload') {
         window.location.reload();
@@ -7175,7 +7175,7 @@ let calcFutureChallenges = function() {
     }
     tempSections = combineJSONs(tempSections, manualSections);
     myWorker2.terminate();
-    myWorker2 = new Worker("./worker.js?v=6.9.65");
+    myWorker2 = new Worker("./worker.js?v=6.9.66");
     myWorker2.onmessage = workerOnMessage;
     myWorker2.postMessage({
         type: 'future',
